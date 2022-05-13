35fec10e37bd3d92bb7b938930da4d7d2e2103e0
Grama Nicolae <gramanicu@gmail.com>
2022-05-14 02:46:35 +0300

    Added auth0

---
+++ server.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import * as dotenv from 'dotenv';
+import { auth } from 'express-oauth2-jwt-bearer';
+
+dotenv.config();
+
+if (!process.env.ISSUER_BASE_URL || !process.env.AUDIENCE) {
+    throw 'Make sure you have ISSUER_BASE_URL, and AUDIENCE in your .env file';
+}
+
+const app = express();
+const PORT = parseInt(process.env.PORT);
+const checkJwt = auth({
+    audience: process.env.AUDIENCE,
+    issuerBaseURL: process.env.ISSUER_BASE_URL,
+});
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
+
+app.get('/api/public', function (req, res) {
+    res.json({
+        message: "Hello from a public endpoint! You don't need to be authenticated to see this.",
+    });
+});
+
+// This route needs authentication
+app.get('/api/private', checkJwt, function (req, res) {
+    res.json({
+        message: 'Hello from a private endpoint! You need to be authenticated to see this.',
+    });
+});
