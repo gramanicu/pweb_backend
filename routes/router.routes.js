94141a73af81a9271109f95520b8874abbedad5b
CatalinLucian <catalinptr3@gmail.com>
2022-05-17 22:17:41 +0300

    added checkJwt middleware

--- routes/router.routes.js
+++ routes/router.routes.js
@@ -3,12 +3,13 @@ import LocationRouter from "./locations.routes.js";
 import UserRouter from "./users.routes.js";
 import ServiceRouter from "./services.routes.js";
 import AccomodationRequestRouter from "./accomodationRequests.routes.js";
+import checkJwt from "../middlewares/checkJwt.js";
 
 const router = express.Router();
 
-router.use('/locations', LocationRouter);
-router.use('/users', UserRouter);
-router.use('/services', ServiceRouter);
-router.use('/accomodation-request', AccomodationRequestRouter);
+router.use('/locations', checkJwt, LocationRouter);
+router.use('/users', checkJwt, UserRouter);
+router.use('/services', checkJwt, ServiceRouter);
+router.use('/accomodation-request', checkJwt, AccomodationRequestRouter);
 
 export default router;
--- server.js
+++ server.js
@@ -3,9 +3,7 @@ import cors from 'cors';
 import helmet from 'helmet';
 import * as dotenv from 'dotenv';
 import router from './routes/router.routes.js';
-import { auth, claimCheck, claimIncludes } from 'express-oauth2-jwt-bearer';
-
-import LocationRouter from './routes/locations.routes.js';
+// import { auth, claimCheck, claimIncludes } from 'express-oauth2-jwt-bearer';
 
 dotenv.config();
 
@@ -15,10 +13,6 @@ if (!process.env.ISSUER_BASE_URL || !process.env.AUDIENCE) {
 
 const app = express();
 const PORT = parseInt(process.env.PORT);
-const checkJwt = auth({
-    audience: process.env.AUDIENCE,
-    issuerBaseURL: process.env.ISSUER_BASE_URL,
-});
 
 app.use(helmet());
 app.use(cors());
@@ -29,20 +23,20 @@ app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`);
 });
 
-app.get('/api/public', function (req, res) {
-    res.json({
-        message: "Hello from a public endpoint! You don't need to be authenticated to see this.",
-    });
-});
+// app.get('/api/public', function (req, res) {
+//     res.json({
+//         message: "Hello from a public endpoint! You don't need to be authenticated to see this.",
+//     });
+// });
 
-// This route needs authentication
-app.get(
-    '/api/private',
-    checkJwt,
-    claimIncludes('permissions', 'create:location', 'view:location'),
-    function (req, res) {
-        res.json({
-            message: 'Hello from a private endpoint! You need to be authenticated to see this.',
-        });
-    }
-);
+// // This route needs authentication
+// app.get(
+//     '/api/private',
+//     checkJwt,
+//     claimIncludes('permissions', 'create:location', 'view:location'),
+//     function (req, res) {
+//         res.json({
+//             message: 'Hello from a private endpoint! You need to be authenticated to see this.',
+//         });
+//     }
+// );
