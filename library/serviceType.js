5e00e4e9dd96323cdb716eaefd46898b5f63221d
CatalinLucian <catalinptr3@gmail.com>
2022-05-17 23:46:10 +0300

    added serviceType enum

---
+++ library/serviceType.js
@@ -0,0 +1,10 @@
+const serviceType = {
+    GENERIC: 'Generic',
+    FOOD: 'Food',
+    CLOTHES: 'Clothes',
+    MEDICAL_SERVICE: 'Medical_service',
+    TRANSLATION_SERVICE: 'Translation_service',
+    LEISURE: 'Leisure'
+}
+
+export default serviceType
