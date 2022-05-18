4544cf023c2d206da09797fd888f5d91bc6baa0d
Grama Nicolae <gramanicu@gmail.com>
2022-05-18 14:22:56 +0300

    Added location to accommodation request

---
+++ prisma/migrations/20220518112241_add_location_to_request/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - Added the required column `locationId` to the `accommodationRequests` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `locationId_owner` to the `accommodationRequests` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE `accommodationrequests` ADD COLUMN `locationId` INTEGER NOT NULL,
+    ADD COLUMN `locationId_owner` INTEGER NOT NULL;
+
+-- AddForeignKey
+ALTER TABLE `accommodationRequests` ADD CONSTRAINT `accommodationRequests_locationId_locationId_owner_fkey` FOREIGN KEY (`locationId`, `locationId_owner`) REFERENCES `locations`(`id`, `id_owner`) ON DELETE RESTRICT ON UPDATE CASCADE;
