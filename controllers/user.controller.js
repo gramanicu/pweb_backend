43b7b6c3d0bb802a59b44a5519cf6db9dcfff043
CatalinLucian <catalinptr3@gmail.com>
2022-05-19 00:15:05 +0300

    Added users/get route definition

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,7 +1,8 @@
 import prisma from "../prismaClient.js";
 
 const addUser = async (req, res) => { return;};
+const getUser = async (req, res) => { return;};
 
-const UserController = { addUser };
+const UserController = { addUser, getUser };
 
 export default UserController;
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -4,5 +4,6 @@ import UserController from '../controllers/user.controller.js';
 const UserRouter = express.Router();
 
 UserRouter.post('/new', UserController.addUser);
+UserRouter.get('/get', UserController.getUser);
 
 export default UserRouter;
